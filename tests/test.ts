6d376e1f0e798d76c361d1c2b4721562d3c45737
范文华 <531362022@qq.com>
2020-04-24 15:11:15 +0800

    tests: Update snapshot

--- tests/test.ts
+++ tests/test.ts
@@ -1,13 +1,10 @@
 export abstract class Demo {
   public props: number = 3;
-
   public props12: number = 3;
-  public props345: number = 3;
-
+  public props345 = 3;
   public/*protected*/ props1 = 3;
   public/*protected*/ readonly props2: number = 3;
   public/*private*/ props3 : string = '3';
-
   public props123: number = 3;
   public props3456: number = 3;
 
